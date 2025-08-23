09bc9494e8f1aa6d868ba6a454a0e7580fb29ca5
Shawn-Strutton <sstrutton@comcast.net>
2025-08-22 18:27:26 -0700

    Add DI extensions and package references for identity
    This commit introduces new package references for
    `Microsoft.Extensions.Configuration.Abstractions` and
    `Microsoft.Extensions.DependencyInjection.Abstractions`
    (version `9.0.8`) in the project files.
    
    New extension methods for dependency injection are added
    in `APIServiceCollectionExtensions.cs`,
    `IdentityApplicationServiceExtensions.cs`,
    `IdentityICollectionServiceExtensions.cs`, and
    `IdentityServiceCollectionExtensions.cs`. These methods
    facilitate the registration of identity-related services,
    enhancing modularity and organization within the application.

---
+++ ST.Core.Identity.DI/Extensions/IdentityICollectionServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using ST.Core.Identity.Application.Extensions;
+using ST.Core.Identity.Infrastructure.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ST.Core.Identity.DI.Extensions
+{
+    public static class IdentityICollectionServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddIdentityInfrastructureServices(configuration);
+            services.AddIdentityApplicationServices(configuration);
+            return services;
+        }
+    }
+}
---
+++ ST.Core.Identity.Infrastructure/Extensions/IdentityServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST.Core.Identity.Infrastructure.Extensions
+{
+    public static class IdentityServiceCollectionExtensions
+    {
+        public static IServiceCollection AddIdentityInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add identity services and configurations here
+            return services;
+        }
+    }
+}
