66c035d3f50c292b8e972b341ca38a16969ef0ad
Shawn-Strutton <sstrutton@comcast.net>
2025-08-25 20:02:13 -0700

    Add UserBase class and update identity management packages
    Updated UserBase.cs to introduce a new UserBase class inheriting from IdentityUser<Guid>, with properties for user management. Modified ST.Core.Identity.Domain.csproj to include a new folder for Authorization entities and added package references for Microsoft.Extensions.Identity.Core and Microsoft.Extensions.Identity.Stores (v9.0.8).

---
+++ ST.Core.Identity.Domain/Authentication/Entities/UserBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST.Core.Identity.Domain.Authentication.Entities
+{
+    public class UserBase : IdentityUser<Guid>
+    {
+        public DateTime CreateDate { get; set; } = DateTime.UtcNow;
+        public string? ProviderName { get; set; } // e.g., Facebook, Google, Local
+        public bool IsDeleted { get; set; } = false;
+        public bool IsActive { get; set; } = true; 
+    }
+}
