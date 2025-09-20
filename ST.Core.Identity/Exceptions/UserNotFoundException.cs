31f8ca1d57b2d19bef22086a04f18306c84d7295
Shawn-Strutton <sstrutton@comcast.net>
2025-09-20 01:57:37 -0700

    Add UserNotFoundException for user validation
    Introduce the `UserNotFoundException` class in the `ST.Core.Identity.Exceptions` namespace to handle cases where a user is expected but not found. The class inherits from `InvalidOperationException` and includes:
    
    - A constructor that formats an exception message with the missing user's ID.
    - A static method `ThrowIfNull<TUser>(TUser? user)` to throw the exception if the user is `null`, with a default `"null"` ID in the message.
    - A static method `ThrowIfNull<TUser>(TUser? user, object userId)` to throw the exception if the user is `null`, including the provided `userId` in the message.

---
+++ ST.Core.Identity/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ST.Core.Identity.Exceptions
+{
+    /// <summary>
+    /// Represents an exception thrown when a user is expected to exist but is not found in the store.
+    /// </summary>
+    public class UserNotFoundException : InvalidOperationException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserNotFoundException"/> class with a formatted message.
+        /// </summary>
+        /// <param name="userId">The ID of the user that was not found.</param>
+        public UserNotFoundException(object userId)
+            : base($"User with ID '{userId}' was not found in the store.") { }
+
+        /// <summary>
+        /// Throws a <see cref="UserNotFoundException"/> if the specified user is null.
+        /// </summary>
+        /// <typeparam name="TUser">The user type.</typeparam>
+        /// <param name="user">The user instance to validate.</param>
+        public static void ThrowIfNull<TUser>(TUser? user)
+            where TUser : class
+        {
+            if (user is null)
+                throw new UserNotFoundException("null");
+        }
+
+        /// <summary>
+        /// Throws a <see cref="UserNotFoundException"/> if the specified user is null,
+        /// using the provided ID for context.
+        /// </summary>
+        /// <typeparam name="TUser">The user type.</typeparam>
+        /// <param name="user">The user instance to validate.</param>
+        /// <param name="userId">The ID to include in the exception message.</param>
+        public static void ThrowIfNull<TUser>(TUser? user, object userId)
+            where TUser : class
+        {
+            if (user is null)
+                throw new UserNotFoundException(userId);
+        }
+    }
+}
