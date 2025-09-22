2eef78509d56f271ac4e7d42cc31c54755b454ea
Shawn-Strutton <sstrutton@comcast.net>
2025-09-22 11:40:28 -0700

    Add UserNotFoundForLoginException class
    Introduce a new exception class `UserNotFoundForLoginException`
    in the `ST.Core.Identity.Exceptions` namespace to handle cases
    where a user is not found during login, indicating registration
    is required.
    
    The class includes:
    - A `UserName` property to store the username.
    - A constructor with a custom error message.
    - XML documentation for clarity.
    
    Added necessary `using` directives for required namespaces.

---
+++ ST.Core.Identity/Exceptions/UserNotFoundForLoginException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST.Core.Identity.Exceptions
+{
+    /// <summary>
+    /// Exception thrown when a user with the specified username is not found in the database during login.
+    /// Indicates that registration is required for the user.
+    /// </summary>
+    public class UserNotFoundForLoginException : Exception
+    {
+        /// <summary>
+        /// Gets the username for which the user was not found.
+        /// </summary>
+        public string UserName { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserNotFoundForLoginException"/> class.
+        /// This exception is triggered when a user with the given username was not found in the database,
+        /// and registration is required.
+        /// </summary>
+        /// <param name="userName">The username that was not found.</param>
+        public UserNotFoundForLoginException(string userName)
+            : base($"No user found for '{userName}'. Registration required.")
+        {
+            UserName = userName;
+        }
+    }
+}
